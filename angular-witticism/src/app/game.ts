817157d532997aa398af4e50600c8c3325eb5fb5
Mariah Legg <mariah.legg@gmail.com>
2022-02-02 16:54:19 -0500

    Add game class

---
+++ angular-witticism/src/app/game.ts
@@ -0,0 +1,10 @@
+import { Player } from "./player";
+
+export class Game {
+    public id: number;
+    public code: string;
+    public round: number;
+    public votes: string;
+    public isActive: boolean;
+    public players: Player[];
+}
